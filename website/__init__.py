0d3cda77cbe8e00cb021ff54a21ee6095c2ac8cb
manursutil <mrodsut@gmail.com>
2025-03-20 15:58:20 +0100

    SQLite to PostgreSQL

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+import os
 from werkzeug.security import generate_password_hash
 from flask_migrate import Migrate
 
@@ -11,7 +12,7 @@ DB_NAME = 'database.db'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'eszt**?gvujbnokmj!gsd'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     migrate = Migrate(app, db)
